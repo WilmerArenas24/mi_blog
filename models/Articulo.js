f42d44f35850fd5b95d4581e09c69e96a6ba7dc1
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-04-28 14:30:28 -0500

    Modelo de Articulo

--- models/Articulo.js
+++ models/Articulo.js
@@ -1,1 +1,22 @@
 const { Schema, model } = require("mongoose");
+
+const ArticuloSchema = Schema({
+    titulo:{
+        type:String,
+        required:true
+    },
+    contenido:{
+        type:String,
+        required:true
+    },
+    fecha:{
+        type:Date,
+        default:Date.now()
+    },
+    imagen:{
+        type:String,
+        default:"default.png"
+    },
+})
+
+module.exports = model("Articulo",ArticuloSchema, "articles") //El ultimo nombre es el nombre de la coleccion
