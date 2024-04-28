5173ff147ef522d2f18d20cf8ca979751bdf3402
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-04-28 13:03:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const {connection}  = require("./database/connection");
+//Importar express para solicitudes http y crear el servidor 
+const express = require("express");
+const cors = require("cors");
+
+//Inicializar app
+console.log("Arrancando la App");
+
+//Conectar a la base de datos
+connection();
+
+// Crear servidor NodeJS
+const app = express();
+const puerto = 3900;
+
+//Configurar el cors
+app.use(cors());
+
+//Convertir body a objeto js
+app.use(express.json());
+
+//Crear rutas
+app.get("/probando", (req, res) =>{
+    return res.status(200).send(
+        `
+        <h1>Creando la API REST</h1>
+        `
+    )
+})
+
+//Escuchar servidor
+app.listen(puerto, () =>{
+    console.log("Servidor corriendo en el puerto:"+puerto);
+})
